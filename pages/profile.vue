129652e66b34b13d335abd7380e997bc2212cf95
fire8327 <fire83274@gmail.com>
2025-10-31 10:16:12 +0300

    upd profile

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,27 +1,35 @@
 <template>
     <div class="flex max-lg:flex-col gap-8 grow">
-        <div class="w-full lg:w-1/3 flex flex-col gap-2">
+        <div class="w-full lg:w-[30%] flex flex-col gap-2">
             <div class="flex items-center gap-4">
                 <p class="font-medium text-xl">user_123456</p>
                 <button class="flex cursor-pointer">
                     <Icon class="text-2xl" name="material-symbols-light:edit"/>
                 </button>
             </div>
+            <div class="flex items-center gap-2">
+                <div class="w-1 h-1 rounded-full bg-green-400"></div>
+                <p class="-mt-1 text-sm font-mono font-medium text-green-400">Подписка активна</p>
+            </div>
             <div class="w-full h-px bg-white/20 my-2"></div>
             <button @click="tabs = 'settings'" class="cursor-pointer flex items-center gap-2 text-gray-400 text-base p-2 rounded-xl transition-all duration-500 hover:bg-[#201e18] hover:text-white">
                 <Icon class="text-xl" name="material-symbols:settings-outline"/>
                 <span>Настройки</span>
             </button>
+            <button @click="tabs = 'subscribtion'" class="cursor-pointer flex items-center gap-2 text-gray-400 text-base p-2 rounded-xl transition-all duration-500 hover:bg-[#201e18] hover:text-white">
+                <Icon class="text-xl" name="fluent:payment-16-regular"/>
+                <span>Подписка</span>
+            </button>
             <button @click="tabs = 'stats'" class="cursor-pointer flex items-center gap-2 text-gray-400 text-base p-2 rounded-xl transition-all duration-500 hover:bg-[#201e18] hover:text-white">
                 <Icon class="text-xl" name="material-symbols:data-usage-rounded"/>
-                <span>Использование</span>
+                <span>Использование и Статистика</span>
             </button>
             <button @click="isSupportModalShow = true" class="cursor-pointer flex items-center gap-2 text-gray-400 text-base p-2 rounded-xl transition-all duration-500 hover:bg-[#201e18] hover:text-white">
                 <Icon class="text-xl" name="ic:round-email"/>
                 <span>Связь с разработчиком</span>
             </button>
         </div>
-        <div class="w-full lg:w-2/3 h-full">
+        <div class="w-full lg:w-[70%] h-full">
             <!-- настройки -->
             <div v-if="tabs === 'settings'" class="flex flex-col gap-6">
                 <p class="text-xl font-mono font-semibold">Настройки</p>
@@ -36,6 +44,15 @@
                     </div>
                 </div>
                 <div class="flex flex-col gap-2">
+                    <p class="font-medium ml-2 text-sm text-gray-400">Техническая информация</p>
+                    <div class="flex max-md:flex-col gap-2 md:justify-between md:items-center rounded-xl border border-white/20 p-4">
+                        <div class="flex flex-col gap-2">
+                            <p class="text-base">ID пользователя</p>
+                        </div>
+                        <button class="bg-white px-4 py-1.5 rounded-xl font-semibold text-sm text-[#131313] transition-all duration-500 hover:opacity-70">Скопировать ID</button>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-2">
                     <p class="font-medium ml-2 text-sm text-gray-400">Ещё</p>
                     <div class="flex max-md:flex-col gap-2 md:justify-between md:items-center rounded-xl border border-white/20 p-4">
                         <div class="flex flex-col gap-2">
@@ -46,9 +63,24 @@
                 </div>
             </div>
 
+            <div v-if="tabs === 'subscribtion'" class="flex flex-col gap-6">
+                <div class="flex flex-col gap-2">
+                    <p class="font-medium ml-2 text-sm text-gray-400">Подписка</p>
+                    <div class="flex max-md:flex-col gap-2 md:justify-between md:items-center rounded-xl border border-white/20 p-4">
+                        <div class="flex flex-col gap-2">
+                            <p class="text-base">Текущий тариф: <span class="font-semibold">Free</span></p>
+                            <p class="text-sm text-gray-400">Действует до: 03.09.2029</p>
+                        </div>
+                        <button class="border border-green-400 text-green-400 px-4 py-1.5 rounded-xl font-semibold text-sm transition-all duration-500 hover:opacity-70">
+                            Активировать
+                        </button>
+                    </div>
+                </div>
+            </div>
+
             <!-- статистика -->
-            <div v-if="tabs === 'stats'" class="flex flex-col gap-8 p-4 rounded-xl border border-white/20">
-                <div class="flex flex-col gap-6">
+            <div v-if="tabs === 'stats'" class="flex flex-col gap-8">
+                <div class="flex flex-col gap-6 p-4 rounded-xl border border-white/20">
                     <div class="flex items-center gap-4">
                         <Icon class="text-3xl" name="fluent:data-usage-16-regular"/>
                         <p class="text-xl md:text-2xl font-mono font-semibold">Статистика пользователя</p>
@@ -69,15 +101,15 @@
                             <p class="text-blue-400">Изображений</p>
                         </div>
                         <div class="flex flex-col gap-2 bg-[#201e18] items-center text-center p-4 rounded-xl">
-                            <div class="flex items-center justify-center p-2 rounded-xl bg-purple-500/10">
-                                <Icon class="text-3xl" name="heroicons:calendar-days-20-solid"/>
+                            <div class="flex items-center justify-center p-2 rounded-xl bg-orange-500/10">
+                            <Icon class="text-3xl" name="ic:round-smart-toy"/>
                             </div>
-                            <p class="text-2xl md:text-3xl font-mono font-semibold">7</p>
-                            <p class="text-purple-400">Дней активности</p>
+                            <p class="text-2xl md:text-3xl font-mono font-semibold">0/5</p>
+                            <p class="text-orange-400">Диалогов</p>
                         </div>
                     </div>
                 </div>
-                <div class="flex flex-col gap-6">
+                <div class="flex flex-col gap-6 p-4 rounded-xl border border-white/20">
                     <p class="text-xl md:text-2xl font-mono font-semibold">Использование</p>
                     <p class="text-center text-sm text-gray-400">В разработке...</p>
                 </div>
@@ -107,6 +139,7 @@
   /* состояния */
   const tabs = ref("settings")
   const isSupportModalShow = ref(false)
+  const { id } = useUserStore()
   
   /* подключение хранилищ */
   const { showMessage } = useMessagesStore()
